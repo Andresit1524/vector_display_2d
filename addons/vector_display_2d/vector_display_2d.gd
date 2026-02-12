49c598308ec4718fa73dac55435b2f26d6b7e425
Andrés López <andreslopez01182009@gmail.com>
2026-02-12 18:43:12 -0500

    Version 1.1
    - Performamce improvements
    - New option: rainbow colors

--- addons/vector_display_2d/vector_display_2d.gd
+++ addons/vector_display_2d/vector_display_2d.gd
@@ -3,7 +3,7 @@ extends Node2D
 const SHORTCUT: InputEventKey = preload("res://addons/vector_display_2d/display_shortcut.tres")
 
 # Constant to improve vanishing speed
-const VANISH_SPEED_CORRECTION := 20
+const VANISH_SPEED_CORRECTION := 10
 
 @export_group("Node")
 @export var target_node: Node ## Node to show its vectors
@@ -15,23 +15,22 @@ const VANISH_SPEED_CORRECTION := 20
 @export var vector_scale: float = 1 ## Change vectors size. This doesn't change the actual vector values
 
 @export_group("Aspect")
+@export_subgroup("Basic")
 @export var main_color: Color = Color.GREEN ## Color for main vector
 @export var x_axis_color: Color = Color.RED ## Color for X component of vector
 @export var y_axis_color: Color = Color.BLUE ## Color for Y component of vector
 @export var width: float = 1 ## Line width
 
-@export_subgroup("Advanced")
+@export_subgroup("Vanish colors")
 @export var vanish_color: bool = false ## If true, the color turns to fallback color when the vector gets short
 @export var vanish_speed: float = 1 ## Vanishing speed for all colors
 @export var fallback_color: Color = Color.BLACK ## Color the vectors tend to when they get short
 
+@export_subgroup("Advanced")
+@export var rainbow: bool = false ## Use rainbow colors based on the vector angle. Only aplies to main vector, not for axes. [b]Not implemented yet[/b]
+
 # Auxiliar variables
 var current_vector := Vector2.ZERO
-var previous_vector: Vector2
-var current_vanish_value: float
-var current_main_color: Color
-var current_x_color: Color
-var current_y_color: Color
 
 # Reassigns the target node when doesn't exist
 func _ready() -> void:
@@ -44,44 +43,51 @@ func _ready() -> void:
 
 # Get the vector from given property
 func _physics_process(_delta) -> void:
-	previous_vector = current_vector
-	current_vector = target_node.get(target_property)
+	var new_vector = target_node.get(target_property)
 
 	# Improves performance rendering when necesary
-	if previous_vector == current_vector:
-		return
+	if current_vector == new_vector: return
 
+	current_vector = new_vector
 	queue_redraw()
 
 # Draw the vectors
 func _draw() -> void:
-	if not show_vectors:
-		return
+	if not show_vectors: return
 
-	_set_colors()
-	current_vanish_value = clampf(vanish_speed * VANISH_SPEED_CORRECTION / current_vector.length(), 0, 1)
+	var colors := _get_draw_colors()
 
 	# Main vector render
-	draw_line(Vector2.ZERO, current_vector * vector_scale, current_main_color, width, true)
+	draw_line(Vector2.ZERO, current_vector * vector_scale, colors.main, width, true)
 
-	if not show_axes:
-		return
+	if not show_axes: return
 
 	# Axes components render
-	draw_line(Vector2.ZERO, Vector2(current_vector.x, 0) * vector_scale, current_x_color, width, true)
-	draw_line(Vector2.ZERO, Vector2(0, current_vector.y) * vector_scale, current_y_color, width, true)
+	draw_line(Vector2.ZERO, Vector2(current_vector.x, 0) * vector_scale, colors.x, width, true)
+	draw_line(Vector2.ZERO, Vector2(0, current_vector.y) * vector_scale, colors.y, width, true)
 
-## Set colors for vectors in both cases (solid color or vanishing color)
-func _set_colors() -> void:
+# Calculates the colors based on current settings (Rainbow, Vanish, etc)
+func _get_draw_colors() -> Dictionary:
+	var result := {
+		"main": main_color,
+		"x": x_axis_color,
+		"y": y_axis_color
+	}
+
+	if rainbow:
+		var angle := current_vector.angle()
+		if angle < 0: angle += TAU
+
+		result.main = Color.from_hsv(angle / TAU, 1.0, 1.0)
+
 	if vanish_color:
-		current_x_color = x_axis_color * (1 - current_vanish_value) + fallback_color * current_vanish_value
-		current_y_color = y_axis_color * (1 - current_vanish_value) + fallback_color * current_vanish_value
-		current_main_color = main_color * (1 - current_vanish_value) + fallback_color * current_vanish_value
-		return
+		var vanish_value := clampf(vanish_speed * VANISH_SPEED_CORRECTION / current_vector.length(), 0.0, 1.0)
 
-	current_x_color = x_axis_color
-	current_y_color = y_axis_color
-	current_main_color = main_color
+		result.x = result.x.lerp(fallback_color, vanish_value)
+		result.y = result.y.lerp(fallback_color, vanish_value)
+		result.main = result.main.lerp(fallback_color, vanish_value)
+
+	return result
 
 # Detects shortcut to toggle visibility
 func _unhandled_key_input(event) -> void:
