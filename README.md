5024210f1601f38d5938908d8a5c1cac9c28b737
Andrés López <andreslopez01182009@gmail.com>
2026-02-15 18:46:59 -0500

    Reorder and polish

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vector display 2D
+# Vector Display 2D
 Inspired by [Easy Vector Display](https://github.com/neropatti/easy_vector_display) by neropatti.
 
 Show vectors on your 2D game with ease, customization and good performance. Some examples:
@@ -12,10 +12,10 @@ Show vectors on your 2D game with ease, customization and good performance. Some
 - Customize and setup colors, scale, and behaviours
 - Enjoy! Use `Shift + V` to quickly toggle visibility during runtime
 - Change the visibility shortcut on [`display shortcut file`](addons/vector_display_2d/display_shortcut.tres) on **Godot editor**
-- Update and customize vectors on runtime with low overhead
+- Update and customize vectors even on runtime with low overhead
 
 ## Planned features
 - Arrowheads and decorators
 - Better customization and performance
 - Autoload access (if needed)
-- 3D compatibility (version 2)
+- 3D compatibility (v2)
--- addons/vector_display_2d/vector_display_2d.gd
+++ addons/vector_display_2d/vector_display_2d.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 const SHORTCUT: InputEventKey = preload("res://addons/vector_display_2d/display_shortcut.tres")
 
-# Constant to improve vanishing speed
-const VANISH_SPEED_CORRECTION := 10
+# Constant to improve dimming speed
+const DIMMING_SPEED_CORRECTION := 10
 
 @export_group("Node")
 @export var target_node: Node ## Node to show its vectors
 @export var target_property: String = "velocity" ## Name of the Vector2 attribute or variable in node's script
 
-@export_group("Rendering")
+@export_group("Show")
 @export var show_vectors: bool = true: ## Show or hide all
 	set(value):
 		show_vectors = value
@@ -18,13 +18,21 @@ const VANISH_SPEED_CORRECTION := 10
 	set(value):
 		show_axes = value
 		queue_redraw()
+
+@export_group("Rendering")
 @export var vector_scale: float = 1: ## Change vectors size. This doesn't change the actual vector values
 	set(value):
 		vector_scale = value
 		queue_redraw()
+@export var width: float = 1: ## Line width
+	set(value):
+		width = value
+		queue_redraw()
+@export var clamp_vector: bool = false ## Clamp the vector length to a max value defined below. This doesn't change the actual vector values
+@export var normalize: bool = false ## Normalize vector length to max length defined below. This doesn't change the actual vector values
+@export var max_length: float = 100 ## Max length for vector clamping or normalizing
 
-@export_group("Aspect")
-@export_subgroup("Basic")
+@export_group("Colors")
 @export var main_color: Color = Color.GREEN: ## Color for main vector
 	set(value):
 		main_color = value
@@ -37,42 +45,33 @@ const VANISH_SPEED_CORRECTION := 10
 	set(value):
 		y_axis_color = value
 		queue_redraw()
-@export var width: float = 1: ## Line width
+@export var rainbow: bool = false: ## Change main vector color based on the its angle. Not aplies for axes
 	set(value):
-		width = value
+		rainbow = value
 		queue_redraw()
 
-@export_subgroup("Vanish colors")
-@export var vanish_color: bool = false: ## If true, the color turns to fallback color when the vector gets short
+@export_group("Color dimming")
+@export var dimming: bool = false: ## Turns the vector color to a fallback one when the vector gets short
 	set(value):
-		vanish_color = value
+		dimming = value
 		queue_redraw()
-@export var vanish_speed: float = 1: ## Vanishing speed for all colors
+@export var dimming_speed: float = 1: ## Dimming speed for all colors
 	set(value):
-		vanish_speed = value
+		dimming_speed = value
 		queue_redraw()
 @export var fallback_color: Color = Color.BLACK: ## Color the vectors tend to when they get short
 	set(value):
 		fallback_color = value
 		queue_redraw()
-@export var vanish_if_normalized: bool = false: ## Apply vanish even when vectors are normalized
+@export var dimming_if_normalized: bool = false: ## Apply dimming even when vectors are normalized
 	set(value):
-		vanish_if_normalized = value
+		dimming_if_normalized = value
 		queue_redraw()
-
-@export_subgroup("Advanced")
-@export var rainbow: bool = false: ## Use rainbow colors based on the vector angle. Only aplies to main vector, not for axes
-	set(value):
-		rainbow = value
-		queue_redraw()
-@export var clamp_vector: bool = false ## Clamp the vector length to a max value. This doesn't change the actual vector values
-@export var normalize: bool = false ## Normalize vectors instead of clamping, at max length defined below. This doesn't change the actual vector values
-@export var max_length: float = 100 ## Max length for vector clamping
 
 # Auxiliar variables
 var current_vector := Vector2.ZERO
 
-# Reassigns the target node when doesn't exist
+# Reassigns the target node or throws error when it doesn't exists
 func _ready() -> void:
 	if target_node == null:
 		push_warning("Target node not defined. Autoassigning to parent node")
@@ -115,7 +114,7 @@ func _draw() -> void:
 	draw_line(Vector2.ZERO, Vector2(current_vector.x, 0) * vector_scale, colors.x, width, true)
 	draw_line(Vector2.ZERO, Vector2(0, current_vector.y) * vector_scale, colors.y, width, true)
 
-# Calculates the colors based on current settings (Rainbow, Vanish, etc)
+## Calculate colors based on current settings (Rainbow, Dimming, etc)
 func _get_draw_colors() -> Dictionary:
 	var result := {
 		"main": main_color,
@@ -129,18 +128,18 @@ func _get_draw_colors() -> Dictionary:
 
 		result.main = Color.from_hsv(angle / TAU, 1.0, 1.0)
 
-	if vanish_color and (not normalize or vanish_if_normalized):
+	if dimming and (not normalize or dimming_if_normalized):
 		var length := current_vector.length()
-		var vanish_value := 1.0
+		var dimming_value := 1.0
 		if not is_zero_approx(length):
-			vanish_value = clampf(vanish_speed * VANISH_SPEED_CORRECTION / length, 0.0, 1.0)
+			dimming_value = clampf(dimming_speed * DIMMING_SPEED_CORRECTION / length, 0.0, 1.0)
 
-		result.x = result.x.lerp(fallback_color, vanish_value)
-		result.y = result.y.lerp(fallback_color, vanish_value)
-		result.main = result.main.lerp(fallback_color, vanish_value)
+		result.x = result.x.lerp(fallback_color, dimming_value)
+		result.y = result.y.lerp(fallback_color, dimming_value)
+		result.main = result.main.lerp(fallback_color, dimming_value)
 
 	return result
 
 # Detects shortcut to toggle visibility
-func _unhandled_key_input(event) -> void:
+func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_pressed() and event.is_match(SHORTCUT): show_vectors = not show_vectors
