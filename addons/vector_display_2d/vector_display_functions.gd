0208d0b0ef99ca80d5cd7c77f452c56d954fa13e
Andrés López <andreslopez01182009@gmail.com>
2026-04-13 13:35:24 -0500

    Optimizations
    1. Corrected typos
    2. Changed signal added to settings resource
    3. Enums and StringNames added to better readability and preformance

--- addons/vector_display_2d/vector_display_functions.gd
+++ addons/vector_display_2d/vector_display_functions.gd
@@ -2,11 +2,20 @@
 class_name VectorDisplayFunctions extends RefCounted
 
 
-#region Initial verifications
+# Enum aliases to avoid writing VectorDisplaySettings every time
+const LenghtModes = VectorDisplaySettings.LenghtModes
+const PivotModes = VectorDisplaySettings.PivotModes
+const AxesPivotModes = VectorDisplaySettings.AxesPivotModes
+const DimmingTypes = VectorDisplaySettings.DimmingTypes
+
+
+#region Initial and auxiliary methods
 
 
 ## Check the target node, its property value and settings resource
-static func check_targets_and_settings(self_node: Node, target_node: Node, target_property: String, settings: VectorDisplaySettings):
+static func check_targets_and_settings(self_node: Node, target_node: Node, target_property: StringName, settings: VectorDisplaySettings):
+	if not settings: push_error("[VectorDisplay] Settings not defined")
+
 	if target_node == null:
 		push_warning("[VectorDisplay] Target node not defined. Autoassigning to parent node")
 		target_node = self_node.get_parent()
@@ -17,24 +26,22 @@ static func check_targets_and_settings(self_node: Node, target_node: Node, targe
 
 	if not target_node.get(target_property) is Vector2:
 		push_error("[VectorDisplay] Target property is not a Vector2 or doesn't exist")
-		return
-
-	if not settings:
-		push_error("[VectorDisplay] Settings not defined")
-		return
 
 
 ## Process a vector to apply lenght mode
-static func apply_lenght_mode(vector, settings: VectorDisplaySettings):
+static func apply_length_mode(vector, settings: VectorDisplaySettings):
+	var result = vector
+
 	match settings.length_mode:
-		"Clamp": return vector.limit_length(settings.max_length)
-		"Normalize": return vector.normalized() * settings.max_length
-		"Normal": return vector
+		LenghtModes.NORMAL: pass
+		LenghtModes.CLAMP: result = vector.limit_length(settings.max_length)
+		LenghtModes.NORMALIZE: result = vector.normalized() * settings.max_length
+		_: push_error("[VectorDisplay] Length mode not supported: %s" % settings.length_mode)
 
-	return null
+	return result * settings.vector_scale
 
 
-## Auxiliar: check vector type
+## Auxiliary: check vector type
 static func _is_vector_type(vector) -> bool:
 	if vector is Vector2 or vector is Vector3: return true
 
@@ -64,6 +71,10 @@ class VDColors:
 
 ## Calculate colors based on current settings (Rainbow, Dimming, etc)
 static func calculate_draw_colors(vector, current_raw_length: float, settings: VectorDisplaySettings) -> VDColors:
+	# Check type and quit if is not vector
+	if not _is_vector_type(vector): return
+
+	# Colors initialization
 	var colors := VDColors.new(
 		settings.main_color,
 		settings.x_axis_color,
@@ -71,27 +82,27 @@ static func calculate_draw_colors(vector, current_raw_length: float, settings: V
 		settings.z_axis_color
 	)
 
-	# Check type, throws error or add new color for 3D if necessary
-	if not _is_vector_type(vector): return colors
-
-	# Color rainbow
+	# Apply color rainbow
 	if settings.rainbow:
 		var angle: float = vector.angle()
 		if angle < 0: angle += TAU
 
 		colors.main = Color.from_hsv(angle / TAU, 1.0, 1.0)
 
-	# Color dimming
-	if settings.dimming and (not settings.length_mode == "Normalize" or settings.normalized_dimming_type != "None"):
-		var length: float = vector.length()
-		match settings.normalized_dimming_type:
-			"Absolute": length = current_raw_length
-			"Visual": length = vector.length()
+	# Apply color dimming
+	if settings.dimming:
+		var length: float
+		match settings.dimming_type:
+			DimmingTypes.ABSOLUTE: length = current_raw_length
+			DimmingTypes.VISUAL, _: length = vector.length()
 
+		# Value
 		var dimming_value := 1.0
-		if not is_zero_approx(length):
-			dimming_value = clampf(settings.dimming_intensity * settings.DIMMING_INTENSITY_CORRECTION / length, 0.0, 1.0)
+		if not is_zero_approx(length): dimming_value = clampf(
+			settings.dimming_intensity * settings.DIMMING_INTENSITY_CORRECTION / length, 0.0, 1.0
+		)
 
+		# Apply
 		colors.x = colors.x.lerp(settings.fallback_color, dimming_value)
 		colors.y = colors.y.lerp(settings.fallback_color, dimming_value)
 		colors.z = colors.z.lerp(settings.fallback_color, dimming_value)
@@ -110,10 +121,13 @@ static func calculate_draw_colors(vector, current_raw_length: float, settings: V
 class VDPosition:
 	var begin: Vector2
 	var end: Vector2
+
 	var x_begin: Vector2
 	var x_end: Vector2
+
 	var y_begin: Vector2
 	var y_end: Vector2
+
 	var z_begin: Vector3
 	var z_end: Vector3
 
@@ -126,13 +140,14 @@ static func get_main_vector_position(vector, settings: VectorDisplaySettings) ->
 	if not _is_vector_type(vector): return current_vector
 
 	match settings.pivot_mode:
-		"Normal":
+		PivotModes.NORMAL:
 			# The rest of calculations can be made directly without worring for type
 			current_vector.begin = Vector2.ZERO if vector is Vector2 else Vector3.ZERO
 			current_vector.end = vector
-		"Centered":
+		PivotModes.CENTERED:
 			current_vector.begin = - vector / 2
 			current_vector.end = vector / 2
+		_: push_error("[VectorDisplay] Pivot mode not supported: %s" % settings.pivot_mode)
 
 	return current_vector
 
@@ -143,13 +158,20 @@ static func get_axes_positions(vector, settings: VectorDisplaySettings) -> VDPos
 
 	# Check type, throws error or add new axes for 3D if necessary
 	if not _is_vector_type(vector): return axes
+
 	if vector is Vector3:
 		axes.z_begin = Vector3.ZERO
 		axes.z_end = Vector3.ZERO
 
+	# Conditions to the next cases
+	var axis_normal := settings.axes_pivot_mode == AxesPivotModes.NORMAL
+	var axis_centered := settings.axes_pivot_mode == AxesPivotModes.CENTERED
+	var same_and_normal := settings.pivot_mode == PivotModes.NORMAL and settings.axes_pivot_mode == AxesPivotModes.SAME
+	var same_and_centered := settings.pivot_mode == PivotModes.CENTERED and settings.axes_pivot_mode == AxesPivotModes.SAME
+
 	# Special case: Centered and Normal Axis
 	# Takes the normal axes ends and then substracts half of original vector
-	if settings.axes_pivot_mode == "Normal" and settings.pivot_mode == "Centered":
+	if axis_normal and settings.pivot_mode == PivotModes.CENTERED:
 		axes.x_begin = - vector / 2
 		axes.x_end = (Vector2(vector.x, 0) if vector is Vector2 else Vector3(vector.x, 0, 0)) - vector / 2
 		axes.y_begin = - vector / 2
@@ -162,7 +184,7 @@ static func get_axes_positions(vector, settings: VectorDisplaySettings) -> VDPos
 		return axes
 
 	# Normal setting: takes the normal components
-	if settings.axes_pivot_mode == "Normal" or (settings.pivot_mode == "Normal" and settings.axes_pivot_mode == "Same"):
+	if axis_normal or same_and_normal:
 		axes.x_begin = Vector2.ZERO if vector is Vector2 else Vector3.ZERO
 		axes.x_end = Vector2(vector.x, 0) if vector is Vector2 else Vector3(vector.x, 0, 0)
 		axes.y_begin = Vector2.ZERO if vector is Vector2 else Vector3.ZERO
@@ -175,7 +197,7 @@ static func get_axes_positions(vector, settings: VectorDisplaySettings) -> VDPos
 		return axes
 
 	# Centered setting: center all axes (- axis / 2, axis / 2)
-	if settings.axes_pivot_mode == "Centered" or (settings.pivot_mode == "Centered" and settings.axes_pivot_mode == "Same"):
+	if axis_centered or same_and_centered:
 		axes.x_begin = - Vector2(vector.x / 2, 0) if vector is Vector2 else -Vector3(vector.x / 2, 0, 0)
 		axes.x_end = Vector2(vector.x / 2, 0) if vector is Vector2 else Vector3(vector.x / 2, 0, 0)
 		axes.y_begin = - Vector2(0, vector.y / 2) if vector is Vector2 else -Vector3(0, vector.y / 2, 0)
